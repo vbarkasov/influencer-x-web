e393b3f578e066b5bf45f4f233a969295233ca49
Nikita Danilov <hello@nikitadanilov.com>
2018-05-02 14:32:45 -0700

    General Update

--- scripts/background.js
+++ scripts/background.js
@@ -1,45 +1,27 @@
 chrome.webNavigation.onCompleted.addListener(function (data) {
-        /*if (data.url && data.url.match(/www\.google\.com/)) {
-			window.open("popup.html", "extension_popup", "width=300,height=400");
-        }*/
-        //console.log(data);
-        jQuery.get("https://welearn.school/wp-json/v1/?s="+data.url, function(edata) {
-        	//try {
 
-        		f = edata; //JSON.parse(edata);
-				console.log(f);
-	        	if('name' in f) {
-        			console.log(11114);
-	        		
-	        		zcode = 'var influencerData=JSON.parse(`'+JSON.stringify(edata)+'`); ';
+	jQuery.get("https://welearn.school/wp-json/v1/?s="+data.url, function(edata) {
+		f = edata;
+		console.log(f);
+		if('name' in f) {
+			
+			zcode = 'var influencerData=JSON.parse(`'+JSON.stringify(edata)+'`); ';
 
+			$.get("./popup.html", function(pd) {
+				chrome.tabs.executeScript(data.tabId,{
+					code: zcode+"var div=document.createElement('div'); document.body.appendChild(div); div.innerHTML=`"+pd+"`;"
+				});
 
-			    	$.get("./popup.html", function(pd) {
-						chrome.tabs.executeScript(data.tabId,{
-					        code: zcode+"var div=document.createElement('div'); document.body.appendChild(div); div.innerHTML=`"+pd+"`;"
-				    	});
+				$.get("./scripts/pscripts.js", function(zd) {
+					chrome.tabs.executeScript(data.tabId,{
+						code: zd
+					});
+					
+				});
 
-						$.get("./scripts/pscripts.js", function(zd) {
-							chrome.tabs.executeScript(data.tabId,{
-						        code: zd
-					    	});
-					    	
-				    	});
+			});
 
-			    	});
-
-	        	}/* else {
-	        		chrome.browserAction.setIcon({
-			            path: "icons/icon-16.png",
-			            tabId: data.tabId
-			        });
-	        	}*/
-        	/*} catch {
-        		console.log(4222);
-        	}*/
-        });
-    },
-    {
-        //url: [{hostContains: '.'}]
-    }
+		}
+	});
+}
 );
