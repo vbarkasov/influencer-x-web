641f0644e712216ebacf171317d7cee5f37ea5c5
macfire10 <macfire10@gmail.com>
2018-05-07 04:20:26 +0300

    Added "Course Preview" option: - Animations - Re-added stars - Reworked pscripts.js to insert data

--- scripts/pscripts.js
+++ scripts/pscripts.js
@@ -25,13 +25,21 @@ function loadUpUiElements () {
 
     var actualCode = '(' + function() {
 
-        var domPopup = document.getElementById('influencer-app'),
-            domFiona = document.getElementById('fiona-btn');
+        var domPopup = document.getElementById('influencer-app')
+            domFiona = document.getElementById('fiona-btn')
+            domCourseInfo = document.getElementById('course-preview')
+            domInfluencerBtn = document.getElementById('last-course-btn')
+            domCourseBackBtn = document.getElementById('hide-course-preview-button')
+            domSocial = document.getElementById('influencer-social')
 
         $(document).on("click", ".fiona-btn", function () {
             window.openInfluencerXPopup();
         }).on("click", "#influencer-app .close-btn", function () {
             window.closeInfluencerXPopup();
+        }).on("click", "#influencer-app .influencer-x-btn", function () {
+            window.openCourseInfo();
+        }).on("click", "#hide-course-preview-button", function () {
+            window.closeCourseInfo();
         });
 
         function closeInfluencerXPopup(){
@@ -41,6 +49,7 @@ function loadUpUiElements () {
                 domPopup.classList.add('hide-popup');
             }, 700);
         }
+
         function openInfluencerXPopup(){
             domPopup.classList.remove('hide-popup');
             domFiona.classList.remove('active-fiona');
@@ -49,8 +58,26 @@ function loadUpUiElements () {
             }, 20);
         }
 
+        function openCourseInfo(){
+            domInfluencerBtn.classList.add('course-btn-hidden');
+            domSocial.classList.add('influencer-social-hidden')
+            domSocial.classList.remove('influencer-social-margin')
+            domCourseInfo.classList.remove('course-preview-hidden');
+            domCourseInfo.classList.add('course-preview-active');
+        }
+
+        function closeCourseInfo(){
+            domInfluencerBtn.classList.remove('course-btn-hidden');
+            domSocial.classList.remove('influencer-social-hidden')
+            domSocial.classList.add('influencer-social-margin')
+            domCourseInfo.classList.add('course-preview-hidden');
+            domCourseInfo.classList.remove('course-preview-active');
+        }
+
         window.openInfluencerXPopup = openInfluencerXPopup;
         window.closeInfluencerXPopup = closeInfluencerXPopup;
+        window.openCourseInfo = openCourseInfo;
+        window.closeCourseInfo = closeCourseInfo;
 
         domPopup.addEventListener( 'click', function( e ) {
             if ( e.target === domPopup ) {
@@ -73,13 +100,31 @@ function insertingDataIntoPopup(avatar, name, website, rating, social, soc2, cou
     var domAva = $('#influencer-app .influencer-info .avatar'),
       domName = jQuery('#influencer-app .influencer-info .name'),
       domSitelink = jQuery('#influencer-app .influencer-info .website'),
-      domCourse = jQuery('#influencer-app .influencer-info .last-course-btn');
+      domRating = jQuery('#influencer-app .course-preview .course-rating'),
+      domCourse = jQuery('#influencer-app .influencer-info .last-course-btn'),
+      domYoutube = jQuery('#influencer-app #youtube-frame'),
+      domCourseCaption = jQuery("#influencer-app .course-caption");
+      domCourseRatingNumber = jQuery("#influencer-app .course-rating-number");
+      domCourseRatingText = jQuery("#influencer-app .course-rating-text");
+      domCoursePrice = jQuery("#influencer-app .course-price");
+    
+    if(rating == ''){ 
+        rating = defaultRating
+    }
 
     domAva.css('background-image', 'url(' + avatar + ')');
     domName.text(name);
     domSitelink.text(website);
     domSitelink.attr('href', website);
+    domRating.addClass('rating-' + rating);
+    domYoutube.attr('src', 'https://www.youtube.com/embed/PTUkTjC77fA')
     domCourse.children().attr('href', course[0].link);
+    domCourseCaption.text('These two programs help people who want to make money and beginner affiliate marketers to get started online. While Super Affiliate System is an upsell of Internet Jetset, it is a completely different training course.')
+    domCourseRatingNumber.text(rating/10)
+    domCourseRatingText.text('(2,050)')
+    domCoursePrice.text('$14.99')
+
+
 
     social.forEach(function(item, i, arr) {
       appendToHTMLSocialItem(item, soc2[i].link);
